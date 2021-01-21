652bcfe368288ee80cdd9dbac12d986560a5a072
David Babayan <40572719+betdaw71@users.noreply.github.com>
2021-01-21 07:15:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,305 @@
+$(window).on("scroll", function () {
+    var scrolled = $(this).scrollTop();
+    if( scrolled > 137 ) {
+        $('.main').addClass('scrolled');
+    }   
+    if( scrolled <= 107 ) {     
+        $('.main').removeClass('scrolled');
+    }
+});
+var headCatalog = false;
+
+$(document).mouseup(function (e){ 
+		var div = $(".wrap-head"); 
+		if (!div.is(e.target) 
+		    && div.has(e.target).length === 0) { 
+			div.fadeOut(200); 
+//            headCatalog = false;
+		}
+	});
+
+$('.header-category').on('click',function(){
+    headCatalog=false;
+    if(headCatalog === false){
+        $('.wrap-head').fadeIn(200);
+        headCatalog = true
+    }else{
+        
+    }
+});
+$('.info__backcall-wrapper').on('click',function(){
+    $('.backcall-form').fadeToggle(100);
+});
+$('.wrapper-close').on('click',function(){
+    $('.backcall-form').fadeOut(100);
+});
+
+$('.minus').on('click',function(){
+    var id = this.id;
+    // parseInt($('.numb'+id).html());
+    if (parseInt($('.numb'+id).html())>1) {
+        
+        $('.numb'+id).html(parseInt($('.numb'+id).html())-1);
+        $('#quantity'+id).val(parseInt($('#quantity'+id).val())-1);
+    }
+});
+$('.plus').on('click',function(){
+    var id = this.id;
+    if (parseInt($('.numb'+id).html())<10) {
+        $('.numb'+id).html(parseInt($('.numb'+id).html())+1);
+        $('#quantity'+id).val(parseInt($('#quantity'+id).val())+1);
+    }
+});
+
+
+
+$(document).ready(function() {
+
+    var sync1 = $("#sync1");
+    var sync2 = $("#sync2");
+    var slidesPerPage = 4; //globaly define number of elements per page
+    var syncedSecondary = true;
+
+    sync1.owlCarousel({
+        items: 1,
+        slideSpeed: 2000,
+        nav: true,
+        autoplay: true, 
+        dots: false,
+        infinity:false,
+        loop: true,
+        autoHeight:true,
+        responsiveRefreshRate: 200,
+        navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    }).on('changed.owl.carousel', syncPosition);
+
+    sync2
+        .on('initialized.owl.carousel', function() {
+            sync2.find(".owl-item").eq(0).addClass("current");
+        })
+        .owlCarousel({
+            items: slidesPerPage,
+            dots: true,
+            nav: true,
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+            responsiveRefreshRate: 100
+        }).on('changed.owl.carousel', syncPosition2);
+
+    function syncPosition(el) {
+        //if you set loop to false, you have to restore this next line
+        //var current = el.item.index;
+
+        //if you disable loop you have to comment this block
+        var count = el.item.count - 1;
+        var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+        if (current < 0) {
+            current = count;
+        }
+        if (current > count) {
+            current = 0;
+        }
+
+        //end block
+
+        sync2
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+        var onscreen = sync2.find('.owl-item.active').length - 1;
+        var start = sync2.find('.owl-item.active').first().index();
+        var end = sync2.find('.owl-item.active').last().index();
+
+        if (current > end) {
+            sync2.data('owl.carousel').to(current, 100, true);
+        }
+        if (current < start) {
+            sync2.data('owl.carousel').to(current - onscreen, 100, true);
+        }
+    }
+
+    function syncPosition2(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            sync1.data('owl.carousel').to(number, 100, true);
+        }
+    }
+
+    sync2.on("click", ".owl-item", function(e) {
+        e.preventDefault();
+        var number = $(this).index();
+        sync1.data('owl.carousel').to(number, 300, true);
+    });
+
+});
+
+//owl2row plugin
+;(function ($, window, document, undefined) {
+    Owl2row = function (scope) {
+        this.owl = scope;
+        this.owl.options = $.extend({}, Owl2row.Defaults, this.owl.options);
+        //link callback events with owl carousel here
+
+        this.handlers = {
+            'initialize.owl.carousel': $.proxy(function (e) {
+                if (this.owl.settings.owl2row) {
+                    this.build2row(this);
+                }
+            }, this)
+        };
+
+        this.owl.$element.on(this.handlers);
+    };
+
+    Owl2row.Defaults = {
+        owl2row: false,
+        owl2rowTarget: 'item',
+        owl2rowContainer: 'owl2row-item',
+        owl2rowDirection: 'utd' // ltr
+    };
+
+    //mehtods:
+    Owl2row.prototype.build2row = function(thisScope){
+    
+        var carousel = $(thisScope.owl.$element);
+        var carouselItems = carousel.find('.' + thisScope.owl.options.owl2rowTarget);
+
+        var aEvenElements = [];
+        var aOddElements = [];
+
+        $.each(carouselItems, function (index, item) {
+            if ( index % 2 === 0 ) {
+                aEvenElements.push(item);
+            } else {
+                aOddElements.push(item);
+            }
+        });
+
+        carousel.empty();
+
+        switch (thisScope.owl.options.owl2rowDirection) {
+            case 'ltr':
+                thisScope.leftToright(thisScope, carousel, carouselItems);
+                break;
+
+            default :
+                thisScope.upTodown(thisScope, aEvenElements, aOddElements, carousel);
+        }
+
+    };
+
+    Owl2row.prototype.leftToright = function(thisScope, carousel, carouselItems){
+
+        var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
+        var owlMargin = thisScope.owl.options.margin;
+
+        var carouselItemsLength = carouselItems.length;
+
+        var firsArr = [];
+        var secondArr = [];
+
+        //console.log(carouselItemsLength);
+
+        if (carouselItemsLength %2 === 1) {
+            carouselItemsLength = ((carouselItemsLength - 1)/2) + 1;
+        } else {
+            carouselItemsLength = carouselItemsLength/2;
+        }
+
+        //console.log(carouselItemsLength);
+
+        $.each(carouselItems, function (index, item) {
+
+
+            if (index < carouselItemsLength) {
+                firsArr.push(item);
+            } else {
+                secondArr.push(item);
+            }
+        });
+
+        $.each(firsArr, function (index, item) {
+            var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
+
+            var firstRowElement = firsArr[index];
+                firstRowElement.style.marginBottom = owlMargin + 'px';
+
+            rowContainer
+                .append(firstRowElement)
+                .append(secondArr[index]);
+
+            carousel.append(rowContainer);
+        });
+
+    };
+
+    Owl2row.prototype.upTodown = function(thisScope, aEvenElements, aOddElements, carousel){
+
+        var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
+        var owlMargin = thisScope.owl.options.margin;
+
+        $.each(aEvenElements, function (index, item) {
+
+            var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
+            var evenElement = aEvenElements[index];
+
+            evenElement.style.marginBottom = owlMargin + 'px';
+
+            rowContainer
+                .append(evenElement)
+                .append(aOddElements[index]);
+
+            carousel.append(rowContainer);
+        });
+    };
+
+    /**
+     * Destroys the plugin.
+     */
+    Owl2row.prototype.destroy = function() {
+        var handler, property;
+
+        for (handler in this.handlers) {
+            this.owl.dom.$el.off(handler, this.handlers[handler]);
+        }
+        for (property in Object.getOwnPropertyNames(this)) {
+            typeof this[property] !== 'function' && (this[property] = null);
+        }
+    };
+
+    $.fn.owlCarousel.Constructor.Plugins['owl2row'] = Owl2row;
+})( window.Zepto || window.jQuery, window,  document );
+//end of owl2row plugin
+
+//init carousel
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 4,
+    margin: 20,
+    arrows:false,
+    nav: false,
+    responsive: {
+    1200: {
+      items: 4
+    },
+    
+    320: {
+      items: 2,
+      margin: 3,
+    }
+
+
+  }
+  });
+  $(".arrow-right").click(function(){
+        owl.trigger("next.owl.carousel");
+    });
+  $(".arrow-left").click(function(){
+        owl.trigger("prev.owl.carousel");
+    });
+});
